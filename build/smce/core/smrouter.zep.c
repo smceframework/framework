b881eea99297658ddfe766c46624a29f28fa110f
root <imadige@gmail.com>
2015-01-29 20:33:14 +0200

    update

--- build/smce/core/smrouter.zep.c
+++ build/smce/core/smrouter.zep.c
@@ -14,30 +14,24 @@
 #include "kernel/main.h"
 #include "kernel/object.h"
 #include "kernel/memory.h"
-#include "kernel/operators.h"
-#include "kernel/array.h"
 #include "kernel/string.h"
+#include "kernel/array.h"
 #include "kernel/fcall.h"
 #include "kernel/hash.h"
+#include "kernel/operators.h"
 #include "kernel/concat.h"
 
-
-/**
- *
- * @author Samed Ceylan
- * @link http://www.samedceylan.com/
- * @copyright 2015 SmceFramework
- * @github https://github.com/smceframework
- */
 ZEPHIR_INIT_CLASS(Smce_Core_SmRouter) {
 
 	ZEPHIR_REGISTER_CLASS(Smce\\Core, SmRouter, smce, core_smrouter, smce_core_smrouter_method_entry, 0);
 
 	zend_declare_property_null(smce_core_smrouter_ce, SL("router"), ZEND_ACC_PROTECTED TSRMLS_CC);
 
-	zend_declare_property_null(smce_core_smrouter_ce, SL("route"), ZEND_ACC_PROTECTED TSRMLS_CC);
+	zend_declare_property_string(smce_core_smrouter_ce, SL("route"), "", ZEND_ACC_PROTECTED TSRMLS_CC);
 
-	zend_declare_property_null(smce_core_smrouter_ce, SL("request"), ZEND_ACC_PROTECTED TSRMLS_CC);
+	zend_declare_property_string(smce_core_smrouter_ce, SL("request"), "", ZEND_ACC_PROTECTED TSRMLS_CC);
+
+	zend_declare_property_null(smce_core_smrouter_ce, SL("requestArray"), ZEND_ACC_PROTECTED TSRMLS_CC);
 
 	return SUCCESS;
 
@@ -94,6 +88,123 @@ PHP_METHOD(Smce_Core_SmRouter, setRoute) {
 
 }
 
+PHP_METHOD(Smce_Core_SmRouter, scriptNameIsFalse) {
+
+	zval *requestArray = NULL, *routeGetEx, *_0, *_1 = NULL;
+
+	ZEPHIR_MM_GROW();
+
+	_0 = zephir_fetch_nproperty_this(this_ptr, SL("requestArray"), PH_NOISY_CC);
+	ZEPHIR_CPY_WRT(requestArray, _0);
+	if (zephir_isset_property(this_ptr, SS("route") TSRMLS_CC)) {
+		ZEPHIR_INIT_VAR(routeGetEx);
+		_0 = zephir_fetch_nproperty_this(this_ptr, SL("route"), PH_NOISY_CC);
+		zephir_fast_explode_str(routeGetEx, SL("/"), _0, LONG_MAX TSRMLS_CC);
+		ZEPHIR_INIT_NVAR(requestArray);
+		array_init_size(requestArray, 3);
+		ZEPHIR_INIT_VAR(_1);
+		if (zephir_array_isset_long(routeGetEx, 0)) {
+			ZEPHIR_OBS_NVAR(_1);
+			zephir_array_fetch_long(&_1, routeGetEx, 0, PH_NOISY, "smce/core/smrouter.zep", 56 TSRMLS_CC);
+		} else {
+			ZEPHIR_INIT_NVAR(_1);
+			ZVAL_STRING(_1, "", 1);
+		}
+		zephir_array_update_string(&requestArray, SL("controller"), &_1, PH_COPY | PH_SEPARATE);
+		ZEPHIR_INIT_LNVAR(_1);
+		if (zephir_array_isset_long(routeGetEx, 1)) {
+			ZEPHIR_OBS_NVAR(_1);
+			zephir_array_fetch_long(&_1, routeGetEx, 1, PH_NOISY, "smce/core/smrouter.zep", 57 TSRMLS_CC);
+		} else {
+			ZEPHIR_INIT_NVAR(_1);
+			ZVAL_STRING(_1, "", 1);
+		}
+		zephir_array_update_string(&requestArray, SL("view"), &_1, PH_COPY | PH_SEPARATE);
+	}
+	RETURN_CCTOR(requestArray);
+
+}
+
+PHP_METHOD(Smce_Core_SmRouter, scriptNameIsTrue) {
+
+	HashTable *_12, *_16;
+	HashPosition _11, _15;
+	int ZEPHIR_LAST_CALL_STATUS;
+	zephir_nts_static zephir_fcall_cache_entry *_1 = NULL;
+	zval *requestArray = NULL, *parse = NULL, *requestGetEx, *key = NULL, *value = NULL, *_0, *_2, *_3 = NULL, *_4, *_5, *_6, *_7, *_8, *_9, *_10, **_13, *_14, **_17;
+
+	ZEPHIR_MM_GROW();
+
+	_0 = zephir_fetch_nproperty_this(this_ptr, SL("requestArray"), PH_NOISY_CC);
+	ZEPHIR_CPY_WRT(requestArray, _0);
+	_0 = zephir_fetch_nproperty_this(this_ptr, SL("request"), PH_NOISY_CC);
+	ZEPHIR_CALL_FUNCTION(&parse, "parse_url", &_1, _0);
+	zephir_check_call_status();
+	ZEPHIR_INIT_VAR(requestGetEx);
+	zephir_array_fetch_string(&_2, parse, SL("path"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 69 TSRMLS_CC);
+	zephir_fast_explode_str(requestGetEx, SL("/"), _2, LONG_MAX TSRMLS_CC);
+	ZEPHIR_INIT_NVAR(requestArray);
+	array_init_size(requestArray, 3);
+	ZEPHIR_INIT_VAR(_3);
+	if (zephir_array_isset_long(requestGetEx, 0)) {
+		ZEPHIR_OBS_NVAR(_3);
+		zephir_array_fetch_long(&_3, requestGetEx, 0, PH_NOISY, "smce/core/smrouter.zep", 72 TSRMLS_CC);
+	} else {
+		ZEPHIR_INIT_NVAR(_3);
+		ZVAL_STRING(_3, "", 1);
+	}
+	zephir_array_update_string(&requestArray, SL("controller"), &_3, PH_COPY | PH_SEPARATE);
+	ZEPHIR_INIT_LNVAR(_3);
+	if (zephir_array_isset_long(requestGetEx, 1)) {
+		ZEPHIR_OBS_NVAR(_3);
+		zephir_array_fetch_long(&_3, requestGetEx, 1, PH_NOISY, "smce/core/smrouter.zep", 73 TSRMLS_CC);
+	} else {
+		ZEPHIR_INIT_NVAR(_3);
+		ZVAL_STRING(_3, "", 1);
+	}
+	zephir_array_update_string(&requestArray, SL("view"), &_3, PH_COPY | PH_SEPARATE);
+	_4 = zephir_fetch_nproperty_this(this_ptr, SL("router"), PH_NOISY_CC);
+	zephir_array_fetch_string(&_5, _4, SL("router"), PH_READONLY, "smce/core/smrouter.zep", 75 TSRMLS_CC);
+	zephir_array_fetch_long(&_6, requestGetEx, 0, PH_READONLY, "smce/core/smrouter.zep", 75 TSRMLS_CC);
+	if (zephir_array_isset(_5, _6)) {
+		_7 = zephir_fetch_nproperty_this(this_ptr, SL("router"), PH_NOISY_CC);
+		zephir_array_fetch_string(&_8, _7, SL("router"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 77 TSRMLS_CC);
+		ZEPHIR_OBS_VAR(_10);
+		zephir_array_fetch_long(&_10, requestGetEx, 0, PH_NOISY, "smce/core/smrouter.zep", 77 TSRMLS_CC);
+		zephir_array_fetch(&_9, _8, _10, PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 77 TSRMLS_CC);
+		zephir_is_iterable(_9, &_12, &_11, 0, 0, "smce/core/smrouter.zep", 84);
+		for (
+		  ; zephir_hash_get_current_data_ex(_12, (void**) &_13, &_11) == SUCCESS
+		  ; zephir_hash_move_forward_ex(_12, &_11)
+		) {
+			ZEPHIR_GET_HMKEY(key, _12, _11);
+			ZEPHIR_GET_HVALUE(value, _13);
+			if (zephir_array_isset_long(requestGetEx, (zephir_get_numberval(key) + 2))) {
+				zephir_array_fetch_long(&_14, requestGetEx, (zephir_get_numberval(key) + 2), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 80 TSRMLS_CC);
+				zephir_array_update_zval(&requestArray, value, &_14, PH_COPY | PH_SEPARATE);
+			}
+		}
+	} else {
+		_7 = zephir_fetch_nproperty_this(this_ptr, SL("router"), PH_NOISY_CC);
+		zephir_array_fetch_string(&_8, _7, SL("router"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 85 TSRMLS_CC);
+		zephir_array_fetch_string(&_9, _8, SL("all"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 85 TSRMLS_CC);
+		zephir_is_iterable(_9, &_16, &_15, 0, 0, "smce/core/smrouter.zep", 91);
+		for (
+		  ; zephir_hash_get_current_data_ex(_16, (void**) &_17, &_15) == SUCCESS
+		  ; zephir_hash_move_forward_ex(_16, &_15)
+		) {
+			ZEPHIR_GET_HMKEY(key, _16, _15);
+			ZEPHIR_GET_HVALUE(value, _17);
+			if (zephir_array_isset_long(requestGetEx, (zephir_get_numberval(key) + 2))) {
+				zephir_array_fetch_long(&_14, requestGetEx, (zephir_get_numberval(key) + 2), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 88 TSRMLS_CC);
+				zephir_array_update_zval(&requestArray, value, &_14, PH_COPY | PH_SEPARATE);
+			}
+		}
+	}
+	RETURN_CCTOR(requestArray);
+
+}
+
 /**
  * run
  * 
@@ -101,127 +212,35 @@ PHP_METHOD(Smce_Core_SmRouter, setRoute) {
  */
 PHP_METHOD(Smce_Core_SmRouter, run) {
 
-	HashTable *_17, *_21;
-	HashPosition _16, _20;
 	int ZEPHIR_LAST_CALL_STATUS;
-	zephir_nts_static zephir_fcall_cache_entry *_7 = NULL;
 	zend_bool _2;
-	zval *requestArray = NULL, *routeGetEx = NULL, *parse = NULL, *requestGetEx, *key = NULL, *value = NULL, *_0, *_1, *_3, *_4, *_5, *_6 = NULL, *_8, *_9, *_10, *_11, *_12, *_13, *_14, *_15, **_18, *_19, **_22;
+	zval *requestArray = NULL, *_0, *_1, *_3, *_4;
 
 	ZEPHIR_MM_GROW();
 
-	ZEPHIR_INIT_VAR(requestArray);
-	array_init_size(requestArray, 3);
-	add_assoc_stringl_ex(requestArray, SS("controller"), SL(""), 1);
-	add_assoc_stringl_ex(requestArray, SS("view"), SL(""), 1);
-	ZEPHIR_OBS_VAR(_0);
-	zephir_read_property_this(&_0, this_ptr, SL("request"), PH_NOISY_CC);
-	if (ZEPHIR_IS_EMPTY(_0)) {
+	_0 = zephir_fetch_nproperty_this(this_ptr, SL("requestArray"), PH_NOISY_CC);
+	ZEPHIR_CPY_WRT(requestArray, _0);
+	ZEPHIR_OBS_VAR(_1);
+	zephir_read_property_this(&_1, this_ptr, SL("request"), PH_NOISY_CC);
+	if (ZEPHIR_IS_EMPTY(_1)) {
 		ZEPHIR_INIT_NVAR(requestArray);
 		array_init_size(requestArray, 3);
 		add_assoc_stringl_ex(requestArray, SS("controller"), SL("site"), 1);
 		add_assoc_stringl_ex(requestArray, SS("view"), SL("index"), 1);
 	} else {
-		ZEPHIR_INIT_VAR(routeGetEx);
-		array_init(routeGetEx);
-		_1 = zephir_fetch_nproperty_this(this_ptr, SL("router"), PH_NOISY_CC);
-		_2 = !zephir_array_isset_string(_1, SS("showScriptName"));
+		_0 = zephir_fetch_nproperty_this(this_ptr, SL("router"), PH_NOISY_CC);
+		_2 = !zephir_array_isset_string(_0, SS("showScriptName"));
 		if (!(_2)) {
 			_3 = zephir_fetch_nproperty_this(this_ptr, SL("router"), PH_NOISY_CC);
-			zephir_array_fetch_string(&_4, _3, SL("showScriptName"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 64 TSRMLS_CC);
+			zephir_array_fetch_string(&_4, _3, SL("showScriptName"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 112 TSRMLS_CC);
 			_2 = ZEPHIR_IS_FALSE(_4);
 		}
 		if (_2) {
-			if (zephir_isset_property(this_ptr, SS("route") TSRMLS_CC)) {
-				ZEPHIR_INIT_NVAR(routeGetEx);
-				_5 = zephir_fetch_nproperty_this(this_ptr, SL("route"), PH_NOISY_CC);
-				zephir_fast_explode_str(routeGetEx, SL("/"), _5, LONG_MAX TSRMLS_CC);
-				ZEPHIR_INIT_NVAR(requestArray);
-				array_init_size(requestArray, 3);
-				ZEPHIR_INIT_VAR(_6);
-				if (zephir_array_isset_long(routeGetEx, 0)) {
-					ZEPHIR_OBS_NVAR(_6);
-					zephir_array_fetch_long(&_6, routeGetEx, 0, PH_NOISY, "smce/core/smrouter.zep", 70 TSRMLS_CC);
-				} else {
-					ZEPHIR_INIT_NVAR(_6);
-					ZVAL_STRING(_6, "", 1);
-				}
-				zephir_array_update_string(&requestArray, SL("controller"), &_6, PH_COPY | PH_SEPARATE);
-				ZEPHIR_INIT_LNVAR(_6);
-				if (zephir_array_isset_long(routeGetEx, 1)) {
-					ZEPHIR_OBS_NVAR(_6);
-					zephir_array_fetch_long(&_6, routeGetEx, 1, PH_NOISY, "smce/core/smrouter.zep", 71 TSRMLS_CC);
-				} else {
-					ZEPHIR_INIT_NVAR(_6);
-					ZVAL_STRING(_6, "", 1);
-				}
-				zephir_array_update_string(&requestArray, SL("view"), &_6, PH_COPY | PH_SEPARATE);
-			}
-		} else {
-			_5 = zephir_fetch_nproperty_this(this_ptr, SL("request"), PH_NOISY_CC);
-			ZEPHIR_CALL_FUNCTION(&parse, "parse_url", &_7, _5);
+			ZEPHIR_CALL_METHOD(&requestArray, this_ptr, "scriptnameisfalse", NULL);
 			zephir_check_call_status();
-			ZEPHIR_INIT_VAR(requestGetEx);
-			zephir_array_fetch_string(&_8, parse, SL("path"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 75 TSRMLS_CC);
-			zephir_fast_explode_str(requestGetEx, SL("/"), _8, LONG_MAX TSRMLS_CC);
-			ZEPHIR_INIT_NVAR(requestArray);
-			array_init_size(requestArray, 3);
-			ZEPHIR_INIT_LNVAR(_6);
-			if (zephir_array_isset_long(requestGetEx, 0)) {
-				ZEPHIR_OBS_NVAR(_6);
-				zephir_array_fetch_long(&_6, requestGetEx, 0, PH_NOISY, "smce/core/smrouter.zep", 78 TSRMLS_CC);
-			} else {
-				ZEPHIR_INIT_NVAR(_6);
-				ZVAL_STRING(_6, "", 1);
-			}
-			zephir_array_update_string(&requestArray, SL("controller"), &_6, PH_COPY | PH_SEPARATE);
-			ZEPHIR_INIT_LNVAR(_6);
-			if (zephir_array_isset_long(requestGetEx, 1)) {
-				ZEPHIR_OBS_NVAR(_6);
-				zephir_array_fetch_long(&_6, requestGetEx, 1, PH_NOISY, "smce/core/smrouter.zep", 79 TSRMLS_CC);
-			} else {
-				ZEPHIR_INIT_NVAR(_6);
-				ZVAL_STRING(_6, "", 1);
-			}
-			zephir_array_update_string(&requestArray, SL("view"), &_6, PH_COPY | PH_SEPARATE);
-			_9 = zephir_fetch_nproperty_this(this_ptr, SL("router"), PH_NOISY_CC);
-			zephir_array_fetch_string(&_10, _9, SL("router"), PH_READONLY, "smce/core/smrouter.zep", 81 TSRMLS_CC);
-			zephir_array_fetch_long(&_11, requestGetEx, 0, PH_READONLY, "smce/core/smrouter.zep", 81 TSRMLS_CC);
-			if (zephir_array_isset(_10, _11)) {
-				_12 = zephir_fetch_nproperty_this(this_ptr, SL("router"), PH_NOISY_CC);
-				zephir_array_fetch_string(&_13, _12, SL("router"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 83 TSRMLS_CC);
-				ZEPHIR_OBS_VAR(_15);
-				zephir_array_fetch_long(&_15, requestGetEx, 0, PH_NOISY, "smce/core/smrouter.zep", 83 TSRMLS_CC);
-				zephir_array_fetch(&_14, _13, _15, PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 83 TSRMLS_CC);
-				zephir_is_iterable(_14, &_17, &_16, 0, 0, "smce/core/smrouter.zep", 89);
-				for (
-				  ; zephir_hash_get_current_data_ex(_17, (void**) &_18, &_16) == SUCCESS
-				  ; zephir_hash_move_forward_ex(_17, &_16)
-				) {
-					ZEPHIR_GET_HMKEY(key, _17, _16);
-					ZEPHIR_GET_HVALUE(value, _18);
-					if (zephir_array_isset_long(requestGetEx, (zephir_get_numberval(key) + 2))) {
-						zephir_array_fetch_long(&_19, requestGetEx, (zephir_get_numberval(key) + 2), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 86 TSRMLS_CC);
-						zephir_array_update_zval(&requestArray, value, &_19, PH_COPY | PH_SEPARATE);
-					}
-				}
-			} else {
-				_5 = zephir_fetch_nproperty_this(this_ptr, SL("router"), PH_NOISY_CC);
-				zephir_array_fetch_string(&_13, _5, SL("router"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 90 TSRMLS_CC);
-				zephir_array_fetch_string(&_14, _13, SL("all"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 90 TSRMLS_CC);
-				zephir_is_iterable(_14, &_21, &_20, 0, 0, "smce/core/smrouter.zep", 96);
-				for (
-				  ; zephir_hash_get_current_data_ex(_21, (void**) &_22, &_20) == SUCCESS
-				  ; zephir_hash_move_forward_ex(_21, &_20)
-				) {
-					ZEPHIR_GET_HMKEY(key, _21, _20);
-					ZEPHIR_GET_HVALUE(value, _22);
-					if (zephir_array_isset_long(requestGetEx, (zephir_get_numberval(key) + 2))) {
-						zephir_array_fetch_long(&_19, requestGetEx, (zephir_get_numberval(key) + 2), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 93 TSRMLS_CC);
-						zephir_array_update_zval(&requestArray, value, &_19, PH_COPY | PH_SEPARATE);
-					}
-				}
-			}
+		} else {
+			ZEPHIR_CALL_METHOD(&requestArray, this_ptr, "scriptnameistrue", NULL);
+			zephir_check_call_status();
 		}
 	}
 	RETURN_CCTOR(requestArray);
@@ -263,14 +282,14 @@ PHP_METHOD(Smce_Core_SmRouter, createUrl) {
 	_1 = zephir_array_isset_string(_0, SS("showScriptName"));
 	if (_1) {
 		_2 = zephir_fetch_nproperty_this(this_ptr, SL("router"), PH_NOISY_CC);
-		zephir_array_fetch_string(&_3, _2, SL("showScriptName"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 116 TSRMLS_CC);
+		zephir_array_fetch_string(&_3, _2, SL("showScriptName"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 135 TSRMLS_CC);
 		_1 = ZEPHIR_IS_TRUE(_3);
 	}
 	if (_1) {
 		ZEPHIR_INIT_VAR(_4);
 		ZEPHIR_CONCAT_VSV(_4, baseUrl, "/", controllerView);
 		zephir_concat_self(&str, _4 TSRMLS_CC);
-		zephir_is_iterable(myArray, &_6, &_5, 0, 0, "smce/core/smrouter.zep", 121);
+		zephir_is_iterable(myArray, &_6, &_5, 0, 0, "smce/core/smrouter.zep", 140);
 		for (
 		  ; zephir_hash_get_current_data_ex(_6, (void**) &_7, &_5) == SUCCESS
 		  ; zephir_hash_move_forward_ex(_6, &_5)
@@ -285,7 +304,7 @@ PHP_METHOD(Smce_Core_SmRouter, createUrl) {
 		ZEPHIR_INIT_LNVAR(_4);
 		ZEPHIR_CONCAT_VSV(_4, baseUrl, "/index.php?route=", controllerView);
 		zephir_concat_self(&str, _4 TSRMLS_CC);
-		zephir_is_iterable(myArray, &_10, &_9, 0, 0, "smce/core/smrouter.zep", 126);
+		zephir_is_iterable(myArray, &_10, &_9, 0, 0, "smce/core/smrouter.zep", 145);
 		for (
 		  ; zephir_hash_get_current_data_ex(_10, (void**) &_11, &_9) == SUCCESS
 		  ; zephir_hash_move_forward_ex(_10, &_9)
@@ -338,14 +357,14 @@ PHP_METHOD(Smce_Core_SmRouter, redirect) {
 	_1 = zephir_array_isset_string(_0, SS("showScriptName"));
 	if (_1) {
 		_2 = zephir_fetch_nproperty_this(this_ptr, SL("router"), PH_NOISY_CC);
-		zephir_array_fetch_string(&_3, _2, SL("showScriptName"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 144 TSRMLS_CC);
+		zephir_array_fetch_string(&_3, _2, SL("showScriptName"), PH_NOISY | PH_READONLY, "smce/core/smrouter.zep", 163 TSRMLS_CC);
 		_1 = ZEPHIR_IS_TRUE(_3);
 	}
 	if (_1) {
 		ZEPHIR_INIT_VAR(_4);
 		ZEPHIR_CONCAT_VSV(_4, baseUrl, "/", controllerView);
 		zephir_concat_self(&str, _4 TSRMLS_CC);
-		zephir_is_iterable(myArray, &_6, &_5, 0, 0, "smce/core/smrouter.zep", 149);
+		zephir_is_iterable(myArray, &_6, &_5, 0, 0, "smce/core/smrouter.zep", 168);
 		for (
 		  ; zephir_hash_get_current_data_ex(_6, (void**) &_7, &_5) == SUCCESS
 		  ; zephir_hash_move_forward_ex(_6, &_5)
@@ -360,7 +379,7 @@ PHP_METHOD(Smce_Core_SmRouter, redirect) {
 		ZEPHIR_INIT_LNVAR(_4);
 		ZEPHIR_CONCAT_VSV(_4, baseUrl, "/index.php?route=", controllerView);
 		zephir_concat_self(&str, _4 TSRMLS_CC);
-		zephir_is_iterable(myArray, &_10, &_9, 0, 0, "smce/core/smrouter.zep", 154);
+		zephir_is_iterable(myArray, &_10, &_9, 0, 0, "smce/core/smrouter.zep", 173);
 		for (
 		  ; zephir_hash_get_current_data_ex(_10, (void**) &_11, &_9) == SUCCESS
 		  ; zephir_hash_move_forward_ex(_10, &_9)
@@ -378,6 +397,21 @@ PHP_METHOD(Smce_Core_SmRouter, redirect) {
 	ZEPHIR_CONCAT_SV(_4, "Location: ", str);
 	ZEPHIR_CALL_FUNCTION(NULL, "header", &_15, _4);
 	zephir_check_call_status();
+	ZEPHIR_MM_RESTORE();
+
+}
+
+PHP_METHOD(Smce_Core_SmRouter, __construct) {
+
+	zval *_0;
+
+	ZEPHIR_MM_GROW();
+
+	ZEPHIR_INIT_VAR(_0);
+	array_init_size(_0, 3);
+	add_assoc_stringl_ex(_0, SS("controller"), SL(""), 1);
+	add_assoc_stringl_ex(_0, SS("view"), SL(""), 1);
+	zephir_update_property_this(this_ptr, SL("requestArray"), _0 TSRMLS_CC);
 	ZEPHIR_MM_RESTORE();
 
 }
--- build/smce/core/smrouter.zep.h
+++ build/smce/core/smrouter.zep.h
@@ -6,9 +6,12 @@ ZEPHIR_INIT_CLASS(Smce_Core_SmRouter);
 PHP_METHOD(Smce_Core_SmRouter, setRequest);
 PHP_METHOD(Smce_Core_SmRouter, setRouter);
 PHP_METHOD(Smce_Core_SmRouter, setRoute);
+PHP_METHOD(Smce_Core_SmRouter, scriptNameIsFalse);
+PHP_METHOD(Smce_Core_SmRouter, scriptNameIsTrue);
 PHP_METHOD(Smce_Core_SmRouter, run);
 PHP_METHOD(Smce_Core_SmRouter, createUrl);
 PHP_METHOD(Smce_Core_SmRouter, redirect);
+PHP_METHOD(Smce_Core_SmRouter, __construct);
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_smce_core_smrouter_setrequest, 0, 0, 1)
 	ZEND_ARG_INFO(0, request)
@@ -38,8 +41,11 @@ ZEPHIR_INIT_FUNCS(smce_core_smrouter_method_entry) {
 	PHP_ME(Smce_Core_SmRouter, setRequest, arginfo_smce_core_smrouter_setrequest, ZEND_ACC_PUBLIC)
 	PHP_ME(Smce_Core_SmRouter, setRouter, arginfo_smce_core_smrouter_setrouter, ZEND_ACC_PUBLIC)
 	PHP_ME(Smce_Core_SmRouter, setRoute, arginfo_smce_core_smrouter_setroute, ZEND_ACC_PUBLIC)
+	PHP_ME(Smce_Core_SmRouter, scriptNameIsFalse, NULL, ZEND_ACC_PROTECTED)
+	PHP_ME(Smce_Core_SmRouter, scriptNameIsTrue, NULL, ZEND_ACC_PROTECTED)
 	PHP_ME(Smce_Core_SmRouter, run, NULL, ZEND_ACC_PUBLIC)
 	PHP_ME(Smce_Core_SmRouter, createUrl, arginfo_smce_core_smrouter_createurl, ZEND_ACC_PUBLIC)
 	PHP_ME(Smce_Core_SmRouter, redirect, arginfo_smce_core_smrouter_redirect, ZEND_ACC_PUBLIC)
+	PHP_ME(Smce_Core_SmRouter, __construct, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
   PHP_FE_END
 };
